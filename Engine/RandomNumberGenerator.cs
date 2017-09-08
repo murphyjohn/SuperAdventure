39055b78685edb5d286822b9a3087def7b20bc71
John Murphy <murphy.john1991@gmail.com>
2017-09-08 22:57:37 +0100

    Add project files.

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        //This creates a Random object (_generator) and uses its Next() method to get a random value between the 
+        //min and max values passed in as parameters
+        private static Random _generator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _generator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
